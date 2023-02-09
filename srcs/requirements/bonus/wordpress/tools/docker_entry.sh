8575ab4967f87c22b8498efa75a3d4a005254059
Kazem <kazemzidane@gmail.com>
2023-02-10 02:17:19 +0400

    bonus was no longer installed redis plugin fixed

--- srcs/requirements/bonus/wordpress/tools/docker_entry.sh
+++ srcs/requirements/bonus/wordpress/tools/docker_entry.sh
@@ -16,8 +16,8 @@ wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-roo
 fi
 
 if ! wp plugin is-installed redis-cache --allow-root --path=/var/www/html/wordpress; then
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
-wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
+wp plugin install --activate redis-cache --allow-root
+wp redis enable --allow-root
 fi
 
 echo "Installation done"
